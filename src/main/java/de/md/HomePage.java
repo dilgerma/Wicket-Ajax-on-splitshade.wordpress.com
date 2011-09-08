481c22caffef8645befbf8d20e680fcb7088aef9
Martin Dilger <martin.dilger@googlemail.com>
2011-09-09 00:14:04 +0200

    new AjaxLink

--- src/main/java/de/md/HomePage.java
+++ src/main/java/de/md/HomePage.java
@@ -1,13 +1,32 @@
 package de.md;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
+import org.apache.wicket.ajax.AjaxEventBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
     public HomePage(final PageParameters parameters) {
     	add(new Label("hello","splitshade.wordpress.com rulez"));
+    	
+    	add(new AjaxLink("ajaxLink"){
+    		@Override
+    		public void onClick(AjaxRequestTarget target) {
+    			target.appendJavaScript("alert('hello world!');");
+    		}
+    	});
+    	Label label = new Label("ajaxText","Ajax Clickable Text");
+    	add(label);
+    	label.add(new AjaxEventBehavior("onclick"){
+
+			@Override
+			protected void onEvent(AjaxRequestTarget target) {
+				target.appendJavaScript("alert('clicked')");
+			}});
     }
 }
